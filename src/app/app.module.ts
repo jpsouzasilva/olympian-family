7ca4214f106392bf0b37a746059846a01bef195d
João Paulo Silva de Souza <joao.paulo.silvasouza@hotmail.com>
2018-01-20 18:21:21 -0300

    chores: modeling the challenge and preparing for action

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { DivinitiesService } from './divinities.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DivinitiesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
